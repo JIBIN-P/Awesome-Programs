7d3e2bad6dd23c47bc46024be96cbd7b24bfec1f
Jibin Philipose <philiposejibin@gmail.com>
2019-06-16 20:30:18 +0530

    refactored the code

--- JavaScript/2.Intermediate/Duplicate Zeros.js
+++ JavaScript/2.Intermediate/Duplicate Zeros.js
@@ -4,14 +4,12 @@ var duplicateZeros = function(arr) {
     console.log(arr);
     for(var i = 0; i < arr.length; i++){
         if(arr[i] === 0){
-            if(i != arr.length - 1){
+            if(i != arr.length - 1 && i+1 <= arr.length){
                 for(var j = arr.length - 1; j >= i+2; j--){
                     arr[j] = arr[j-1];
                 }
-                if(i+1 <= arr.length){
                     arr[i+1] = 0;
                     i++;
-                }
             }
         }
     }
